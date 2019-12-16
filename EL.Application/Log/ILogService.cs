16c562f41faacc176fea87fbe6c7a04b61f2b44b
aaron <969340992@qq.com>
2019-12-16 13:55:07 +0800

    提交修改

---
+++ EL.Application/Log/ILogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EL.Entity;
+
+namespace EL.Application
+{
+    public interface ILogService
+    {
+        Task SaveException(Exception ex);
+    }
+}
---
+++ EL.Application/Log/LogService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EL.Entity;
+using EL.Repository;
+
+namespace EL.Application
+{
+    public class LogService : ILogService
+    {
+        private readonly IBaseRepository<LogEntity> _logRepository;
+        public LogService(IBaseRepository<LogEntity> logRepository)
+        {
+            _logRepository = logRepository;
+        }
+        public async Task SaveException(Exception ex)
+        {
+            var entity = new LogEntity
+            {
+                Message = ex.Message,
+                StackTrace = !string.IsNullOrWhiteSpace(ex.StackTrace) ? ex.StackTrace : null,
+                Exception = ex.ToString(),
+                CreateTime = DateTime.Now
+            };
+            await _logRepository.AddEntityAsync(entity);
+            await _logRepository.CommitAsync();
+        }
+    }
+}
