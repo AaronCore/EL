67080ea73d30b784bf96c22616fea7c2f4e07b02
Aaron <969340992@qq.com>
2020-02-24 16:58:54 +0800

    edit

--- EL.Api/Controllers/WeatherForecastController.cs
+++ EL.Api/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EL.Api.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -24,6 +25,7 @@ namespace EL.Api.Controllers
         }
 
         [HttpGet]
+        //[ModelBinder(BinderType =typeof(ArrayModelBinder))]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
